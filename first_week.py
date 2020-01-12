7334064e8b1167a54e1f87209e93a9b6ebd227fa
ngocdinh1410 <56566863+ngocdinh1410@users.noreply.github.com>
2020-01-12 05:09:04 -0500

    Update first_week.py

--- first_week.py
+++ first_week.py
@@ -1,4 +1,4 @@
-#<h1>First Assignment, write some thing here</h1>
+<h1> First assignment, write something here </h1>
 import pandas as pd
 import numpy as np
 print('Hello Capstone Project Course!')
